74435e0ee1a6fb22489daeabd5db68386a28e2fa
Wayne Malcolm Hafely II <wmalcolmh@gmail.com>
2020-11-18 11:30:43 -0500

    switched windows build to dx9

--- src/test_imgui/build_win32.bat
+++ src/test_imgui/build_win32.bat
@@ -1,10 +1,4 @@
 @REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
-set OUT_DIR=Debug
-set OUT_EXE=win_test_imgui
+mkdir Debug
 set IMGUI_DIR=imgui
-set SDL2_DIR=D:\Programs\SDL2-2.0.12
-set INCLUDES=/I%IMGUI_DIR%\backends /I%IMGUI_DIR% /I%SDL2_DIR%\include /I%IMGUI_DIR%\examples\libs\gl3w
-set SOURCES=main_ui.cpp platform_main.cpp %IMGUI_DIR%\backends\imgui_impl_sdl.cpp %IMGUI_DIR%\backends\imgui_impl_opengl3.cpp %IMGUI_DIR%\imgui*.cpp %IMGUI_DIR%\examples\libs\gl3w\GL\gl3w.c
-set LIBS=/libpath:%SDL2_DIR%\lib\x86 SDL2.lib SDL2main.lib opengl32.lib
-mkdir %OUT_DIR%
-cl /nologo /Zi /MD %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS% /subsystem:console
+cl /nologo /Zi /MD /I %IMGUI_DIR%\backends /I %IMGUI_DIR% /I "%DXSDK_DIR%/Include" /D UNICODE /D _UNICODE dx9_win32_platform_main.cpp main_ui.cpp %IMGUI_DIR%\backends\imgui_impl_dx9.cpp %IMGUI_DIR%\backends\imgui_impl_win32.cpp %IMGUI_DIR%\imgui*.cpp /FeDebug/win_test_imgui.exe /FoDebug/ /link /LIBPATH:"%DXSDK_DIR%/Lib/x86" d3d9.lib
