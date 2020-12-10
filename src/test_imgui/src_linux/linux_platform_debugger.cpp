99469f2194b9e15ec77c06d697dcaf3cb079a3e8
wmh <wmalcolmh@gmail.com>
2020-12-10 02:22:28 -0500

    added some implementation to the linux processes list, still does not display properly

--- src/test_imgui/src_linux/linux_platform_debugger.cpp
+++ src/test_imgui/src_linux/linux_platform_debugger.cpp
@@ -1,11 +1,35 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+
 #include "debugger.h"
+
 
 int Debugger::get_number_registers(){
         return 10;
 }
 
 unsigned long Debugger::list_of_processes(Debugger::Process* out_processes, unsigned long max){
-        return 0;
+
+        struct dirent* de_DirEntity = 0;
+        DIR* dir_proc = 0;
+        unsigned long num_processes = 0;
+
+        dir_proc = opendir("/proc");
+        if(dir_proc == 0){
+                return 0;
+        }
+
+        while( (de_DirEntity = readdir(dir_proc)) && num_processes < max){
+                char c = de_DirEntity->d_name[0];
+                for(int i = 0; i < 50 && c != 0; i++){
+                    c = de_DirEntity->d_name[i];
+                    out_processes[num_processes].short_name[i] = c;
+                }
+                num_processes++;
+        }
+
+        return num_processes;
 }
