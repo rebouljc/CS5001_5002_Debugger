025ec7d3fe5cb3b3f09590c36335492cde79c26b
Wayne Malcolm Hafely II <wmalcolmh@gmail.com>
2020-12-12 16:28:52 -0500

    Changed window title name

--- src/test_imgui/src_windows/dx9_win32_platform_main.cpp
+++ src/test_imgui/src_windows/dx9_win32_platform_main.cpp
@@ -33,7 +33,7 @@ int main(int, char**)
     //ImGui_ImplWin32_EnableDpiAwareness();
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
     ::RegisterClassEx(&wc);
-    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("Hello Hunter, Dear ImGui DirectX9 Example"), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T("EasyDebugger"), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
 
     // Initialize Direct3D
     if (!CreateDeviceD3D(hwnd))
